feaf1cd7f2b088b06bbbb7506ae7cc29620dbb6a
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-11-14 18:03:31 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -1,1 +1,10 @@
+<?php
+require_non("util.db.php");
+require_once(("model-course.php"));
 
+$PageTitle = "Courses";
+include "view-header.php";
+$courses = select.courses();
+include "view-courses.php";
+include "view-footer.php";
+?>
