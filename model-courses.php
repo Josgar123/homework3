d6a04f09ee9183fa8e838c92b4539d7d23c929af
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-11-14 18:09:40 -0600

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectCourses() {
+try {
+$conn = get_db_connections ();
+$stmt = $conn->prepare("Select course_id, course_number course_description from 'course'");
+$stmt-> execute();
+$result = $stmt-> get_result();
+$conn->close();
+return $result;
+} catch (Exception $e) {
+$conn-> close();
+throw $e;
+}
+}
+?>
